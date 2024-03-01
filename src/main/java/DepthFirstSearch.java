41cbad224504e5be510ddf0bf5576877d6cbf92a
Adam <adam.ewing.nz@gmail.com>
2024-03-01 08:37:02 +0800

    First commit.

--- src/main/java/DepthFirstSearch.java
+++ src/main/java/DepthFirstSearch.java
@@ -1,2 +1,93 @@
-package PACKAGE_NAME;public class DepthFirstSearch {
+import java.util.Stack;
+
+public class DepthFirstSearch {
+
+    // Main method. Used for visualising and debugging purposes.
+    public static void main(String[] args) {
+
+        int[][] graph1 = {
+                {6,9},
+                {11},
+                {6,7},
+                {9},
+                {5,8},
+                {4,7},
+                {0,2,11},
+                {2,5},
+                {4,10},
+                {0,3},
+                {8},
+                {1,6}
+        };
+
+        int[][] graph2 = {
+                {3,6,5,9},
+                {11},
+                {7,10},
+                {0},
+                {5},
+                {0,4,7},
+                {0,11},
+                {2,5,8},
+                {7},
+                {0},
+                {2},
+                {1,6}
+        };
+
+        int[] path_graph_1 = depthFirstSearch(graph1);
+        int[] path_graph_2 = depthFirstSearch(graph2);
+
+        printPath(path_graph_1);
+        printPath(path_graph_2);
+
+    }
+
+    public static int[] depthFirstSearch(int[][] graph) {
+
+        // Create a stack.
+
+        // Create an array called visited. This will keep track of which nodes we have visited.
+
+        // Create an array called path. This will keep track of the order of nodes that we visit.
+
+        // Create an index for the path array.
+
+        // Push our starting node to the stack. We can begin our traversal from any valid node. Let's begin our traversal at node 0.
+
+        // Record the starting node as visited.
+
+        // While our stack is not empty i.e. while we still have nodes to explore ...
+
+            // Pop the node that we are currently visiting from the stack.
+
+            // Add the node that we are currently visiting to the path.
+
+            // Obtain an array of all neighbouring/adjacent nodes of the node that we are currently visiting.
+
+            // For each neighbouring/adjacent node ...
+
+                // If the neighbouring/adjacent node has not been visited ...
+
+                    // Record the neighbouring/adjacent node as visited.
+
+                    // Push the neighbouring/adjacent node onto the stack.
+
+
+
+
+        // Return the path.
+        return null;
+
+    }
+
+    // A private helper method that prints the path. Used for visualisation and debugging purposes.
+    private static void printPath(int[] path) {
+
+        for (int node: path) System.out.print(node + " ");
+        System.out.println();
+
+    }
+
+
 }
--- src/test/java/DepthFirstSearchTest.java
+++ src/test/java/DepthFirstSearchTest.java
@@ -1,4 +1,84 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 class DepthFirstSearchTest {
-  
+
+    private int[][] graph1;
+    private int[][] graph2;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+        graph1 = new int[][] {
+                {6,9},
+                {11},
+                {6,7},
+                {9},
+                {5,8},
+                {4,7},
+                {0,2,11},
+                {2,5},
+                {4,10},
+                {0,3},
+                {8},
+                {1,6}
+        };
+
+        graph2 = new int[][] {
+                {3,6,5,9},
+                {11},
+                {7,10},
+                {0},
+                {5},
+                {0,4,7},
+                {0,11},
+                {2,5,8},
+                {7},
+                {0},
+                {2},
+                {1,6}
+        };
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+        graph1 = null;
+        graph2 = null;
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void depthFirstSearch() {
+
+        int[] path1 = DepthFirstSearch.depthFirstSearch(graph1);
+        int[] path2 = DepthFirstSearch.depthFirstSearch(graph2);
+
+        assertEquals(0, path1[0]);
+        assertEquals(9, path1[1]);
+        assertEquals(3, path1[2]);
+        assertEquals(6, path1[3]);
+        assertEquals(11, path1[4]);
+        assertEquals(1, path1[5]);
+        assertEquals(2, path1[6]);
+        assertEquals(7, path1[7]);
+        assertEquals(5, path1[8]);
+        assertEquals(4, path1[9]);
+        assertEquals(8, path1[10]);
+        assertEquals(10, path1[11]);
+
+        assertEquals(0, path2[0]);
+        assertEquals(9, path2[1]);
+        assertEquals(5, path2[2]);
+        assertEquals(7, path2[3]);
+        assertEquals(8, path2[4]);
+        assertEquals(2, path2[5]);
+        assertEquals(10, path2[6]);
+        assertEquals(4, path2[7]);
+        assertEquals(6, path2[8]);
+        assertEquals(11, path2[9]);
+        assertEquals(1, path2[10]);
+        assertEquals(3, path2[11]);
+
+    }
 }
